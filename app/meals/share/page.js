fe611a7d311a3417ae4f23177f86b9e06b5a1b92
iremserenay <irem@serenay.net.tr>
2024-04-05 14:16:26 +0300

    creating meal storing updated images and storing data in the database

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,20 +1,8 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
-
-    const meal = {
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-    };
-    console.log(meal);
-  }
   return (
     <>
       <header className={classes.header}>
