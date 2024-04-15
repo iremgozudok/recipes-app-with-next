fe063ef1cee85d52127f80c0d1a0941007a3d1af
iremserenay <irem@serenay.net.tr>
2024-04-15 13:34:29 +0300

    triggering cache revalidations

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <main className="not-found">
       <h1>Page not found</h1>
-      <p>Sorry, we couldn't find the page you were looking for.</p>
+      <p>{`Sorry, we couldn't find the page you were looking for.`}</p>
     </main>
   );
 }
